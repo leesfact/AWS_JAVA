99c48d4c22c92dc2025091835dfc6b1550eb39df
leesfact <lky110408@gmail.com>
2023-03-13 17:38:01 +0900

    23.03.13 오전(HTML,CSS,JavaScript) & 오후 (Python) 커밋

---
+++ web_basic/css_study/javascript/js/js1.js
@@ -0,0 +1,25 @@
+//alert("알림창을 확인해주세요.");
+//alert("두번째 알림창");
+//alert(10+20);
+
+// console.log("println");
+// let flag = confirm("선택해주세요");
+// console.log(flag);
+// let name2 = prompt("이름을 입력해주세요");
+// console.log(name2);
+
+// 일반 자료형
+
+
+//첫번째 방법
+let A = parseInt(prompt("첫번째 값을 입력해주세요."));
+let B = parseInt(prompt("두번째 값을 입력해주세요."));
+
+console.log(A+B);
+
+//두번째 방법
+
+let C = Number(prompt("첫번째 값을 입력해주세요."));
+let D = Number(prompt("두번째 값을 입력해주세요."));
+
+console.log(C+D);
